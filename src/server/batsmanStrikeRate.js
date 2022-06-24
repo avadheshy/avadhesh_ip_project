6e60b401e070a2ecd25f5d5d3eab615239c4d173
avadheshy <avadheshy2015@gmail.com>
2022-06-24 23:10:46 +0530

    a new file added

--- src/server/batsmanStrikeRate.js
+++ src/server/batsmanStrikeRate.js
@@ -10,59 +10,61 @@ function readMatchDataForstrikeRate() {
         eachMatch=matchArray[match].split(',')
         let id= parseInt(eachMatch[idIndex])
         let season=eachMatch[seasonIndex]
-        if(matchId[season]!=undefined){
-            matchId[season].add(id)
+        if(matchId[season]!==undefined){
+            matchId[season].push(id)
         }
         else{
-            matchId[season]=new Set()
-            matchId[season].add(id)
+            matchId[season]=[]
+            matchId[season].push(id)
         }
     }
-    const fileReader = require("fs");
-    matchObject = fileReader.readFileSync("../data/deliveries.csv")
+    const fileReader1 = require("fs");
+    matchObject = fileReader1.readFileSync("../data/deliveries.csv")
     var deliveriesArray = matchObject.toString().split("\n");
     let headers = deliveriesArray[0].split(",")
     let batsmanIndex = 6;
-    let runIndex =18;
+    let runIndex =17;
     let batsmanData= {};
     for (let matchRow = 2; matchRow < deliveriesArray.length - 1; matchRow++){
         eachDelivery=deliveriesArray[matchRow].split(',')
-        let nuns=eachDelivery[runIndex]
+        let runs=parseInt(eachDelivery[runIndex])
         let batsman=eachDelivery[batsmanIndex]
-        let id = eachDelivery[idIndex]
+        let id = parseInt(eachDelivery[idIndex])
         let season=0
-        for(let year in matchId)
-        {
-            if(id in matchId[year])
+      
+        for(const year of Object.keys(matchId))
+        { 
+    
+            if( matchId[year].includes(id))
             {
-                season=year
+                season=year        
             }
+            
         }
-        if (dismissal.length > 0 && dismissal!= 'run out')
+        console.log(season)
+        if(batsmanData[season]!=undefined)
         {
-            if(playerDismissaldata[batsman]!== undefined)
+            if(batsmanData[season][batsman]!=undefined)
             {
-                if(playerDismissaldata[batsman][bowler]!== undefined)
-                playerDismissaldata[batsman][bowler]+=1
-                else
-                playerDismissaldata[batsman][bowler]=1
+                batsmanData[season][batsman][0]+=1
+                batsmanData[season][batsman][1]+=runs
             }
-        
-        else{
-            playerDismissaldata[batsman]={}
-            playerDismissaldata[batsman][bowler]=1
+            else{
+                batsmanData[season][batsman]=[1,runs]
+            }
+         
+
+        }
+        else
+        {
+            batsmanData[season]={}
+            batsmanData[season][batsman]=[1,runs]
         }
     }
 
-    }
-    let playerDismissal=[]
-    for(let i in playerDismissaldata)
-    {
-        obj=playerDismissaldata[i]
-        let max_key=Object.keys(obj).reduce(function(a, b){ return obj[a] > obj[b] ? a : b });
-        playerDismissal.push([i,max_key,obj[max_key]]) 
-    }
-    let json = JSON.stringify(playerDismissal);
+
+    //console.log( batsmanData)
+    /*let json = JSON.stringify(playerDismissal);
     fileReader.writeFileSync('playerDismissal.json', json)*/
   
   
